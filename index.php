12d9d3e1e0902c5ba7b5dd0a2fe3a5ae2723eb24
Timothé Pellen <timothe.pellen@gmail.com>
2026-04-01 16:38:22 +0200

    better

--- index.php
+++ index.php
@@ -25,6 +25,82 @@
                 <a href="asset/download/CV.pdf" download>CV</a>
             </article>
         </div>
+
+                <article>
+                    <h2>Compétences acquises</h2>
+                    <h3 class="normal">Durant mes études, j'ai participé à des projets tous très intéressants qui m'ont apporté <br>
+                    En voici la liste :</h3><br>
+                </article>
+
+            <article class="row">
+                <h3 class="col-12">Réalisation d'un développement d'application</h3>
+                <article class="col-12 col-lg-6 projets">
+                    <p><strong>L'objectif :</strong> créer un jeu de Sudoku de n'importe quelle dimension de grille en langage C jouable sur terminal.</p>
+                    <a href="sudoku.html" class="boutton">le code</a><br>
+                    <img class="demo" src="asset/images/S1.01sudoku.png" alt="sudoku">
+                </article>
+                <article class="col-12 col-lg-6 projets">
+                    <p><strong>L'objectif :</strong> créer un jeu de snake avec des pavées et des trous qui téléporte le serpent en langage C jouable sur terminal.</p>
+                    <a href="snake.html" class="boutton">le code</a><br>
+                    <img class="demo" src="asset/images/S1.01snake.png" alt="snake">
+                </article>
+            </article>
+            <article class="row">
+                <h3 class="col-12">Optimisation des applications informatiques</h3>
+                <article class="col-12 col-lg-6 projets">
+                    <p><strong>L'objectif :</strong> créer un résolveur de Sudoku à partir du programme précédemment créé.</p>
+                    <a href="Backtracking.html" class="boutton">le code</a><br>
+                    <img class="demo" src="asset/images/sae1.02.png" alt="Backtracking">
+                </article>
+                <article class="col-12 col-lg-6 projets">
+                    <p><strong>L'objectif :</strong> créer un snake qui va chercher ses pommes automatiquement à partir du programme de snake déjà fait précédemment</p>
+                    <a href="autoSnake.html" class="boutton">le code</a><br>
+                    <img class="demo" src="asset/images/sae1.02snake.png" alt="snake automatique">
+                </article>
+            </article>
+            <div>
+                <article class="row">
+                    <h3 class="col-12">Installation d'un poste pour le développement</h3>
+                    <article class="col-12 projets">
+                        <p><strong>L'objectif :</strong> réaliser un programme qui permet de créer la documentation d'un fichier écrit en langage C, en HTML et Markdown grâce à du PHP.</p>
+                        <a href="Docker.html" class="boutton">explication</a><br>
+                        <img class="demo" src="asset/images/sae1.03.png" alt="Documentation">
+                    </article>
+                </article>
+            </div>
+            <div>
+                <article class="row">
+                    <h3 class="col-12">Création d’un site Web</h3>
+                    <article class="col-12 col-lg-6 projets">
+                        <p><strong>L'objectif :</strong> créer un site web par groupe de 4 pour une entreprise factice créant du contenu de streaming pour une série au choix. Notre groupe a choisi Scooby-Doo.</p>
+                        <a href="S1.05 old/Backstage.html" class="boutton">ma page</a><br>
+                        <img class="demo" src="asset/images/S1.05scooby-doo.png" alt="HTML">
+                    </article>
+                    <article class="col-12 col-lg-6 projets">
+                        <p><strong>L'objectif :</strong> créer un site web par groupe de 4 pour les Jeux Olympics et plus présisément sur une disipline au choix.</p>
+                        <a href="SAE1.05/Discipline.html" class="boutton">ma page</a><br>
+                        <img class="demo" src="asset/images/S1.05volley-ball.png" alt="HTML">
+                    </article>
+                </article>
+            </div>
+            <div>
+                <article class="row">
+                    <h3 class="col-12">Création personnels</h3>
+                    <article class="col-12 col-lg-6 projets">
+                        <p>J'ai créer un petit jeu de labyrinth seul pour mon été 2025.</p>
+                        <a href="S1.05 old/Backstage.html" class="boutton">ma page</a><br>
+                        <img class="demo" src="asset/images/S1.05scooby-doo.png" alt="HTML">
+                    </article>
+                    <article class="col-12 col-lg-6 projets">
+                        <p><strong>L'objectif :</strong> créer un site web par groupe de 4 pour les Jeux Olympics et plus présisément sur une disipline au choix.</p>
+                        <a href="SAE1.05/Discipline.html" class="boutton">ma page</a><br>
+                        <img class="demo" src="asset/images/S1.05volley-ball.png" alt="HTML">
+                    </article>
+                </article>
+            </div>
+        </section>
+        
+        
     </main>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
